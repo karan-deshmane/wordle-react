67c2a85578637cc3bc39d9136c1618b7bf7e6cb8
Karan Deshmane <karan.deshmane09.kd@gmail.com>
2025-08-23 17:27:01 +0530

    Added word lists and guess lists

---
+++ src/assets/wordList.js
@@ -0,0 +1,5 @@
+
+export const easyWordList = ["ball", "gate", "fish", "road", "milk"];
+export const mediumWordList = ["plant", "crane", "brick", "chair", "touch"];
+export const hardWordList = ["planet", "rocket", "submit", "strike", "jumper"];
+
